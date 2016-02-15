7c326add8d737d19b95f0930dc73c5a6d0d2a6b1
Stefan Schwarz <stefan.schwarz@stud.fh-luebeck.de>
2016-02-15 19:29:27 +0100

    StoryBoard Support

--- WhiteboardMenu/core/print_api.php
+++ WhiteboardMenu/core/print_api.php
@@ -5,11 +5,11 @@ class print_api
    public function printWhiteboardMenu()
    {
       echo '<table align="center">';
-      echo '<tr">';
+      echo '<tr>';
 
-      if (  plugin_is_installed( 'UserProjectView' )
-         && file_exists ( config_get_global ( 'plugin_path' ) . 'UserProjectView' )
-         )
+      if ( plugin_is_installed( 'UserProjectView' )
+         && file_exists( config_get_global( 'plugin_path' ) . 'UserProjectView' )
+      )
       {
          echo '<td>';
          echo '| ';
@@ -17,13 +17,23 @@ class print_api
          echo '</td>';
       }
 
-      if (  plugin_is_installed( 'SpecManagement' )
-         && file_exists ( config_get_global ( 'plugin_path' ) . 'SpecManagement' )
-         )
+      if ( plugin_is_installed( 'SpecManagement' )
+         && file_exists( config_get_global( 'plugin_path' ) . 'SpecManagement' )
+      )
       {
          echo '<td>';
          echo '| ';
          echo '<a href="' . plugin_page( 'choose_document', false, 'SpecManagement' ) . '">' . plugin_lang_get( 'menu_title', 'SpecManagement' ) . '</a>';
+         echo '</td>';
+      }
+
+      if ( plugin_is_installed( 'StoryBoard' )
+         && file_exists( config_get_global( 'plugin_path' ) . 'StoryBoard' )
+      )
+      {
+         echo '<td>';
+         echo '| ';
+         echo '<a href="' . plugin_page( 'storyboard_index', false, 'StoryBoard' ) . '">' . plugin_lang_get( 'menu_title', 'StoryBoard' ) . '</a>';
          echo '</td>';
       }
 
