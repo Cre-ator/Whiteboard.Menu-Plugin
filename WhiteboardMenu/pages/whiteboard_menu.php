b8208f0ac838ad804259199d5960d481eb61c3b9
Stefan Schwarz <s.schwarz@online.de>
2016-04-15 14:55:12 +0200

    bugfix

--- WhiteboardMenu/pages/whiteboard_menu.php
+++ WhiteboardMenu/pages/whiteboard_menu.php
@@ -4,7 +4,7 @@ require_once WHITEBOARDMENU_CORE_URI . 'whiteboard_print_api.php';
 $whiteboard_print_api = new whiteboard_print_api();
 html_page_top1( plugin_lang_get( 'menu_title' ) );
 html_page_top2();
-if ( access_get_local_level( auth_get_current_user_id(), helper_get_current_project() ) >= plugin_config_get( 'AccessLevel' ) )
+if ( access_get_local_level( auth_get_current_user_id(), helper_get_current_project() ) >= plugin_config_get( 'AccessLevel' ) || user_is_administrator( auth_get_current_user_id() ) )
 {
    $whiteboard_print_api->printWhiteboardMenu();
 }
