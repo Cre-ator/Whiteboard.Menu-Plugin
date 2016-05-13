5778a7fcbe80474f6f436069a129615351adc91a
Stefan Schwarz <s.schwarz@online.de>
2016-05-13 18:07:27 +0200

    security issue fix

--- WhiteboardMenu/core/whiteboard_config_api.php
+++ WhiteboardMenu/core/whiteboard_config_api.php
@@ -2,20 +2,20 @@
 
 class whiteboard_config_api
 {
-   public function printFormTitle( $colspan, $lang_string )
+   public function printFormTitle ( $colspan, $lang_string )
    {
       echo '<tr>';
       echo '<td class="form-title" colspan="' . $colspan . '">';
-      echo plugin_lang_get( $lang_string );
+      echo plugin_lang_get ( $lang_string );
       echo '</td>';
       echo '</tr>';
    }
 
-   public function printTableRow()
+   public function printTableRow ()
    {
-      if ( substr( MANTIS_VERSION, 0, 4 ) == '1.2.' )
+      if ( substr ( MANTIS_VERSION, 0, 4 ) == '1.2.' )
       {
-         echo '<tr ' . helper_alternate_class() . '>';
+         echo '<tr ' . helper_alternate_class () . '>';
       }
       else
       {
@@ -23,63 +23,86 @@ class whiteboard_config_api
       }
    }
 
-   public function printCategoryField( $colspan, $rowspan, $lang_string )
+   public function printCategoryField ( $colspan, $rowspan, $lang_string )
    {
       echo '<td class="category" colspan="' . $colspan . '" rowspan="' . $rowspan . '">';
-      echo plugin_lang_get( $lang_string );
+      echo plugin_lang_get ( $lang_string );
       echo '</td>';
    }
 
-   public function printRadioButton( $colspan, $name )
+   public function printRadioButton ( $colspan, $name )
    {
       echo '<td width="100px" colspan="' . $colspan . '">';
       echo '<label>';
       echo '<input type="radio" name="' . $name . '" value="1"';
-      echo ( ON == plugin_config_get( $name ) ) ? 'checked="checked"' : '';
-      echo '/>' . lang_get( 'yes' );
+      echo ( ON == plugin_config_get ( $name ) ) ? 'checked="checked"' : '';
+      echo '/>' . lang_get ( 'yes' );
       echo '</label>';
       echo '<label>';
       echo '<input type="radio" name="' . $name . '" value="0"';
-      echo ( OFF == plugin_config_get( $name ) ) ? 'checked="checked"' : '';
-      echo '/>' . lang_get( 'no' );
+      echo ( OFF == plugin_config_get ( $name ) ) ? 'checked="checked"' : '';
+      echo '/>' . lang_get ( 'no' );
       echo '</label>';
       echo '</td>';
    }
 
-   public function printSpacer( $colspan )
+   public function printSpacer ( $colspan )
    {
       echo '<tr>';
       echo '<td class="spacer" colspan="' . $colspan . '">&nbsp;</td>';
       echo '</tr>';
    }
 
-   public function updateValue( $value, $constant )
+   public function updateValue ( $value, $constant )
    {
       $act_value = null;
 
-      if ( is_int( $value ) )
+      if ( is_int ( $value ) )
       {
-         $act_value = gpc_get_int( $value, $constant );
+         $act_value = gpc_get_int ( $value, $constant );
       }
 
-      if ( is_string( $value ) )
+      if ( is_string ( $value ) )
       {
-         $act_value = gpc_get_string( $value, $constant );
+         $act_value = gpc_get_string ( $value, $constant );
       }
 
-      if ( plugin_config_get( $value ) != $act_value )
+      if ( plugin_config_get ( $value ) != $act_value )
       {
-         plugin_config_set( $value, $act_value );
+         plugin_config_set ( $value, $act_value );
       }
    }
 
-   public function updateButton( $config )
+   public function updateButton ( $config )
    {
-      $button = gpc_get_int( $config );
+      $button = gpc_get_int ( $config );
 
-      if ( plugin_config_get( $config ) != $button )
+      if ( plugin_config_get ( $config ) != $button )
       {
-         plugin_config_set( $config, $button );
+         plugin_config_set ( $config, $button );
+      }
+   }
+
+   /**
+    * Get a plugin configuration option.
+    * @param $p_option
+    * @param $t_basename
+    * @param null $p_default
+    * @param bool $p_global
+    * @param null $p_user
+    * @param null $p_project
+    * @return float|int|mixed|null|string
+    */
+   function whitebaord_plugin_config_get ( $p_option, $t_basename, $p_default = null, $p_global = false, $p_user = null, $p_project = null )
+   {
+      $t_full_option = 'plugin_' . $t_basename . '_' . $p_option;
+      if ( $p_global )
+      {
+         return config_get_global ( $t_full_option, $p_default );
+      }
+      else
+      {
+         return config_get ( $t_full_option, $p_default, $p_user, $p_project );
       }
    }
 }
--- WhiteboardMenu/core/whiteboard_print_api.php
+++ WhiteboardMenu/core/whiteboard_print_api.php
@@ -1,44 +1,61 @@
 <?php
+require_once WHITEBOARDMENU_CORE_URI . 'whiteboard_config_api.php';
 
 class whiteboard_print_api
 {
-   public function printWhiteboardMenu()
+   public function printWhiteboardMenu ()
    {
+      $project_id = helper_get_current_project ();
+      $user_id = auth_get_current_user_id ();
+      $whiteboard_config_api = new whiteboard_config_api();
+
       echo '<table align="center">';
       echo '<tr>';
 
-      if ( plugin_is_installed( 'UserProjectView' )
-         && file_exists( config_get_global( 'plugin_path' ) . 'UserProjectView' )
+      if ( plugin_is_installed ( 'UserProjectView' )
+         && file_exists ( config_get_global ( 'plugin_path' ) . 'UserProjectView' )
       )
       {
-         echo '<td>';
-         echo '| ';
-         echo '<a href="' . plugin_page( 'UserProject', false, 'UserProjectView' ) . '&sortVal=userName&sort=ASC">' . plugin_lang_get( 'menu_userprojecttitle', 'UserProjectView' ) . '</a>';
-         echo '</td>';
+         $user_project_access_level = $whiteboard_config_api->whitebaord_plugin_config_get ( 'UserProjectAccessLevel', 'UserProjectView' );
+         if ( ( user_get_access_level ( $user_id, $project_id ) >= $user_project_access_level ) || user_is_administrator ( $user_id ) )
+         {
+            echo '<td>';
+            echo '| ';
+            echo '<a href="' . plugin_page ( 'UserProject', false, 'UserProjectView' ) . '&sortVal=userName&sort=ASC">' . plugin_lang_get ( 'menu_userprojecttitle', 'UserProjectView' ) . '</a>';
+            echo '</td>';
+         }
       }
 
-      if ( plugin_is_installed( 'SpecManagement' )
-         && file_exists( config_get_global( 'plugin_path' ) . 'SpecManagement' )
+      if ( plugin_is_installed ( 'SpecManagement' )
+         && file_exists ( config_get_global ( 'plugin_path' ) . 'SpecManagement' )
       )
       {
-         echo '<td>';
-         echo '| ';
-         echo '<a href="' . plugin_page( 'choose_document', false, 'SpecManagement' ) . '">' . plugin_lang_get( 'menu_title', 'SpecManagement' ) . '</a>';
-         echo '</td>';
-         echo '<td>';
-         echo '| ';
-         echo '<a href="' . plugin_page( 'manage_versions', false, 'SpecManagement' ) . '">' . plugin_lang_get( 'manversions_thead', 'SpecManagement' ) . '</a>';
-         echo '</td>';
+         $specmanagement_access_level = $whiteboard_config_api->whitebaord_plugin_config_get ( 'AccessLevel', 'SpecManagement' );
+         if ( ( user_get_access_level ( $user_id, $project_id ) >= $specmanagement_access_level ) || user_is_administrator ( $user_id ) )
+         {
+            echo '<td>';
+            echo '| ';
+            echo '<a href="' . plugin_page ( 'choose_document', false, 'SpecManagement' ) . '">' . plugin_lang_get ( 'menu_title', 'SpecManagement' ) . '</a>';
+            echo '</td>';
+            echo '<td>';
+            echo '| ';
+            echo '<a href="' . plugin_page ( 'manage_versions', false, 'SpecManagement' ) . '">' . plugin_lang_get ( 'manversions_thead', 'SpecManagement' ) . '</a>';
+            echo '</td>';
+         }
       }
 
-      if ( plugin_is_installed( 'StoryBoard' )
-         && file_exists( config_get_global( 'plugin_path' ) . 'StoryBoard' )
+      if ( plugin_is_installed ( 'StoryBoard' )
+         && file_exists ( config_get_global ( 'plugin_path' ) . 'StoryBoard' )
       )
       {
-         echo '<td>';
-         echo '| ';
-         echo '<a href="' . plugin_page( 'storyboard_index', false, 'StoryBoard' ) . '">' . plugin_lang_get( 'menu_title', 'StoryBoard' ) . '</a>';
-         echo '</td>';
+         $storyboard_access_level = $whiteboard_config_api->whitebaord_plugin_config_get ( 'AccessLevel', 'StoryBoard' );
+         if ( ( user_get_access_level ( $user_id, $project_id ) >= $storyboard_access_level ) || user_is_administrator ( $user_id ) )
+         {
+            echo '<td>';
+            echo '| ';
+            echo '<a href="' . plugin_page ( 'storyboard_index', false, 'StoryBoard' ) . '">' . plugin_lang_get ( 'menu_title', 'StoryBoard' ) . '</a>';
+            echo '</td>';
+         }
       }
 
       echo '<td>';
