d7574e9107a836f5ee40f47a82deb1d430e93a7d
Stefan Schwarz <s.schwarz@online.de>
2016-06-29 14:05:55 +0200

    bug fixes, version management integration

--- WhiteboardMenu/pages/config_update.php
+++ WhiteboardMenu/pages/config_update.php
@@ -1,14 +1,12 @@
 <?php
-auth_reauthenticate();
-access_ensure_global_level( config_get( 'AccessLevel' ) );
-form_security_validate( 'plugin_WhiteboardMenu_config_update' );
+require_once ( __DIR__ . '/../core/whiteboard_config_api.php' );
 
-require_once WHITEBOARDMENU_CORE_URI . 'whiteboard_constant_api.php';
-require_once WHITEBOARDMENU_CORE_URI . 'whiteboard_config_api.php';
+auth_reauthenticate ();
+access_ensure_global_level ( config_get ( 'AccessLevel' ) );
+form_security_validate ( 'plugin_WhiteboardMenu_config_update' );
 
-$whiteboard_config_api = new whiteboard_config_api();
-$whiteboard_config_api->updateButton( 'show_in_footer' );
-$whiteboard_config_api->updateButton( 'show_menu' );
+whiteboard_config_api::updateButton ( 'show_in_footer' );
+whiteboard_config_api::updateButton ( 'show_menu' );
 
-form_security_purge( 'plugin_WhiteboardMenu_config_update' );
-print_successful_redirect( plugin_page( 'config_page', true ) );
+form_security_purge ( 'plugin_WhiteboardMenu_config_update' );
+print_successful_redirect ( plugin_page ( 'config_page', true ) );
