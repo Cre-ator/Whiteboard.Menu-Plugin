f9f5184f1f8f9591033c06e29314290f4efe3a7d
Stefan Schwarz <s.schwarz@online.de>
2016-09-09 15:34:31 +0200

    new db based management

--- WhiteboardMenu/core/whiteboard_constant_api.php
+++
@@ -1,4 +0,0 @@
-<?php
-define( 'WHITEBOARDMENU_PLUGIN_URL', config_get_global( 'path' ) . 'plugins/' . plugin_get_current() . '/' );
-define( 'WHITEBOARDMENU_PLUGIN_URI', config_get_global( 'plugin_path' ) . plugin_get_current() . DIRECTORY_SEPARATOR );
-define( 'WHITEBOARDMENU_CORE_URI', WHITEBOARDMENU_PLUGIN_URI . 'core' . DIRECTORY_SEPARATOR );
--- WhiteboardMenu/pages/whiteboard_menu.php
+++ WhiteboardMenu/pages/whiteboard_menu.php
@@ -1,7 +1,9 @@
 <?php
-require_once ( __DIR__ . '/../core/whiteboard_print_api.php' );
+require_once ( __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'wmApi.php' );
+
+echo '<link rel="stylesheet" href="plugins/WhiteboardMenu/files/whiteboardmenu.css"/>';
 
 html_page_top1 ( plugin_lang_get ( 'menu_title' ) );
 html_page_top2 ();
-whiteboard_print_api::printWhiteboardMenu ();
+wmApi::printWhiteboardMenu ();
 html_page_bottom1 ();
